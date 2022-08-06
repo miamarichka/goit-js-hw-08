aa4a0483daf0c2fe4807ee1ed06d56098bfc5430
miamarichka <ilchyshyn.marichka@gmail.com>
2022-08-06 22:38:04 +0300

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from "simplelightbox";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+
+function createGalleryItems(newGalleryItems){
+    return newGalleryItems.map(({preview, original, description})=>
+    `<a class="gallery__item" href= ${original}>
+    <img
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+  />
+  </a>`).join('');
+}
+gallery.innerHTML  = createGalleryItems(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a' , {
+    captionsClass: 'gallery__image',
+    captionsData: 'alt',
+    captionDelay: '250'
+});
+
 
 console.log(galleryItems);
