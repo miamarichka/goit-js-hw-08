b046c531ea966acbc6143ea0463c066cde9010cc
miamarichka <ilchyshyn.marichka@gmail.com>
2022-08-06 21:24:18 +0300

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,12 +1,5 @@
-import SimpleLightbox from "simplelightbox";
-
-// Дополнительный импорт стилей
-import "simplelightbox/dist/simple-lightbox.min.css";
-
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// // Дополнительный импорт стилей
-// import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
