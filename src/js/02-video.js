fe1ae2eee100ded21da61dacdd41437fc6a70403
miamarichka <ilchyshyn.marichka@gmail.com>
2022-08-07 20:20:46 +0300

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+function setLocalstorage ({seconds}) {
+    console.log(seconds)
+    localStorage.getItem('videoplayer-current-time', seconds);
+};
+const currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(currentTime);
+
+player.on('timeupdate', throttle(setLocalstorage));
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const email = document.querySelector('input');
+const textarea = document.querySelector('textarea')
+const submit = document.querySelector('button')
 
+email.addEventListener('input', onEmail);
+textarea.addEventListener('input', onTextarea);
+submit.addEventListener('submit', onSubmitBtn);
+
+function onEmail(event){
+    const emailadress = event.target.value;
+    console.log(emailadress)
+    localStorage.setItem('email' , emailadress);
+};
+
+ function onTextarea(event){
+    const textareainput = event.target.value;
+    console.log(textareainput)
+    localStorage.setItem('textarea' , textareainput);
+};
+
+function restore(){
+    const savedEmail = localStorage.getItem('email');
+    const savedTextarea = localStorage.getItem('textarea');
+
+    if(savedEmail){
+        email.value = savedEmail;
+    };
+    if(savedTextarea){
+        textarea.value = savedTextarea;
+    }
+}
